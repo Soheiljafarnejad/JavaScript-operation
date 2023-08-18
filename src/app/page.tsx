bd36e9c0ea4de3f20ba206116e1c2a441b03dfdc
Soheil <jafarnejad.official@gmail.com>
2023-08-18 15:03:15 +0330

    Add tabel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+
+type OperatorType = "+" | "-" | "*" | "**" | "/" | "==" | "===";
+
+const options = [
+  0,
+  "0",
+  [0],
+  [],
+  {},
+  "",
+  null,
+  [null],
+  undefined,
+  NaN,
+  [NaN],
+  "a",
+  Infinity,
+  1,
+  "1",
+  [1],
+  true,
+  false,
+  "true",
+  "false",
+];
+
+const operator: OperatorType[] = ["+", "-", "*", "**", "/", "==", "==="];
+
+const calculation = (first: any, second: any, operator: OperatorType) => {
+  if (operator === "+") return first + second;
+  else if (operator === "-") return first - second;
+  else if (operator === "/") return first / second;
+  else if (operator === "*") return first * second;
+  else if (operator === "**") return first ** second;
+  else if (operator === "==") return first == second;
+  else if (operator === "===") return first === second;
+};
+
+const showResult = (first: any, second: any, operator: OperatorType) => {
+  const result = calculation(first, second, operator);
+  if (result === true) return <span className="bg-green-200 text-green-500">true</span>;
+  else if (result === false) return <span className="bg-red-200 text-red-700">false</span>;
+  else if (typeof result === "string")
+    return <span className="bg-purple-200 text-purple-700">{result === "" ? `""` : result}</span>;
+  else if (typeof result === "number") return <span className="bg-amber-200 text-amber-700">{result}</span>;
+};
+
+const showLabel = (item: any) => {
+  if (item !== null && item !== undefined) {
+    const stringify = item.toString();
+    if (Array.isArray(item) && item.length) return `[${item[0]}]`;
+    else if (stringify === "NaN" || stringify === "Infinity") return stringify;
+    else return `${JSON.stringify(item)}`;
+  } else return `${JSON.stringify(item)}`;
+};
+
+const Page = () => {
+  const [value, setValue] = useState<OperatorType>("==");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="p-4">
+      <h1 className="centering text-4xl mb-4">JavaScript coercion rules</h1>
+
+      <ul className="centering gap-4 max-w-3xl mx-auto mb-4">
+        {operator.map((item) => {
+          return (
+            <li
+              key={item}
+              onClick={() => setValue(item)}
+              className={`text-xl rounded-md transition-all duration-400  ${
+                value === item ? "bg-blue-50" : "bg-white/50"
+              } centering h-10 w-10 cursor-pointer`}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="centering">
+        <div className="overflow-auto p-4">
+          <table>
+            <thead>
+              <tr>
+                <th className="text-2xl">{value}</th>
+                {options.map((item, index) => {
+                  return (
+                    <th key={`1-${index}-${value}`} className="p-3">
+                      {showLabel(item)}
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {options.map((bodyItem, index) => {
+                return (
+                  <tr key={`2-${index}-${value}`}>
+                    <th className="px-3">{showLabel(bodyItem)}</th>
+                    {options.map((headerItem, titleIndex) => {
+                      return <td key={`3-${headerItem}-${titleIndex}-${value}`}>{showResult(bodyItem, headerItem, value)}</td>;
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
+    </section>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
