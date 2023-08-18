b31c851519860cd4a79c9a595aa56540988d121b
Soheil <jafarnejad.official@gmail.com>
2023-08-18 18:28:37 +0330

    add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type OperatorType = "+" | "-" | "*" | "**" | "/" | "==" | "===";
 
-const options = [
+const bodyOptions = [
+  "body1",
+  "body2",
+  "body3",
+  "body4",
+  "body5",
+  "body6",
+  "body7",
+  "body8",
+  "body9",
+  "body10",
+  "body11",
+  "body12",
+  "body13",
+  "body14",
+  "body15",
+  "body16",
+  "body17",
+  "body18",
+  "body19",
+  "body20",
+];
+
+const headerOptions = [
+  "header1",
+  "header2",
+  "header3",
+  "header4",
+  "header5",
+  "header6",
+  "header7",
+  "header8",
+  "header9",
+  "header10",
+  "header11",
+  "header12",
+  "header13",
+  "header14",
+  "header15",
+  "header16",
+  "header17",
+  "header18",
+  "header19",
+  "header20",
+];
+
+const operator: OperatorType[] = ["+", "-", "*", "**", "/", "==", "==="];
+
+const defaultValue = [
   0,
   "0",
   [0],
@@ -27,7 +75,54 @@ const options = [
   "false",
 ];
 
-const operator: OperatorType[] = ["+", "-", "*", "**", "/", "==", "==="];
+const convertToString = (value: any): string => {
+  if (typeof value === "string") {
+    return `"${value}"`;
+  } else if (typeof value === "number" || typeof value === "boolean") {
+    return value.toString();
+  } else if (Array.isArray(value)) {
+    return value.length ? `[${value[0]}]` : `[]`;
+  } else if (value === null) {
+    return "null";
+  } else if (value === undefined) {
+    return "undefined";
+  } else if (isNaN(value) && typeof value !== "object") {
+    return "NaN";
+  } else if (value === Infinity) {
+    return "Infinity";
+  } else if (typeof value === "object") {
+    return "{}";
+  } else {
+    return String(value);
+  }
+};
+
+const convertToPars = (value: any): null | undefined | object | [] | string | number | boolean => {
+  if (/(['"]).*\1/g.test(value)) {
+    const replaceValue = value.replace(/(['"])["']{2}\1/g, "");
+    return JSON.parse(replaceValue);
+  } else if (value.includes("[") && value.includes("]")) {
+    const firstIndex = value.indexOf("[") + 1;
+    let lastIndex = value.indexOf(",");
+    if (lastIndex === -1) lastIndex = value.indexOf("]");
+    const indexOneValue = value.slice(firstIndex, lastIndex);
+    return indexOneValue ? [convertToPars(indexOneValue)] : [];
+  } else if (value.includes("{") && value.includes("}")) {
+    return {};
+  } else if (value === "null") {
+    return null;
+  } else if (value === "undefined") {
+    return undefined;
+  } else if (value === "NaN") {
+    return NaN;
+  } else if (value === "Infinity") {
+    return Infinity;
+  } else if (value && (value === "false" || value === "true" || !isNaN(value))) {
+    return JSON.parse(value);
+  } else {
+    return value;
+  }
+};
 
 const calculation = (first: any, second: any, operator: OperatorType) => {
   if (operator === "+") return first + second;
@@ -48,17 +143,51 @@ const showResult = (first: any, second: any, operator: OperatorType) => {
   else if (typeof result === "number") return <span className="bg-amber-200 text-amber-700">{result}</span>;
 };
 
-const showLabel = (item: any) => {
-  if (item !== null && item !== undefined) {
-    const stringify = item.toString();
-    if (Array.isArray(item) && item.length) return `[${item[0]}]`;
-    else if (stringify === "NaN" || stringify === "Infinity") return stringify;
-    else return `${JSON.stringify(item)}`;
-  } else return `${JSON.stringify(item)}`;
-};
+const defaultHeader = headerOptions.reduce((prev, current, index) => {
+  prev = { ...prev, [current]: convertToString(defaultValue[index]) };
+  return prev;
+}, {});
+
+const defaultBody = bodyOptions.reduce((prev, current, index) => {
+  prev = { ...prev, [current]: convertToString(defaultValue[index]) };
+  return prev;
+}, {});
 
 const Page = () => {
-  const [value, setValue] = useState<OperatorType>("==");
+  const [data, setData] = useState<{ body: {}; header: {}; operator: OperatorType }>({
+    body: defaultBody,
+    header: defaultHeader,
+    operator: "==",
+  });
+
+  const [hederValue, setHeaderValue] = useState(defaultHeader);
+  const [bodyValue, setBodyValue] = useState(defaultBody);
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+    if (name.includes("header")) setHeaderValue((prev) => ({ ...prev, [name]: value }));
+    else setBodyValue((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onClick = (item: OperatorType) => {
+    let body = {};
+    let header = {};
+
+    for (let key in bodyValue) body[key] = convertToPars(bodyValue[key]);
+
+    for (let key in hederValue) header[key] = convertToPars(hederValue[key]);
+
+    let _bodyValue = {};
+    let _headerValue = {};
+
+    for (let key in body) _bodyValue[key] = convertToString(body[key]);
+    for (let key in header) _headerValue[key] = convertToString(header[key]);
+
+    setBodyValue(_bodyValue);
+    setHeaderValue(_headerValue);
+
+    setData({ operator: item, body, header });
+  };
 
   return (
     <section className="p-4">
@@ -69,9 +198,9 @@ const Page = () => {
           return (
             <li
               key={item}
-              onClick={() => setValue(item)}
+              onClick={() => onClick(item)}
               className={`text-xl rounded-md transition-all duration-400  ${
-                value === item ? "bg-blue-50" : "bg-white/50"
+                data.operator === item ? "bg-blue-50" : "bg-white/50"
               } centering h-10 w-10 cursor-pointer`}
             >
               {item}
@@ -85,23 +214,39 @@ const Page = () => {
           <table>
             <thead>
               <tr>
-                <th className="text-2xl">{value}</th>
-                {options.map((item, index) => {
+                <th className="text-2xl">{data.operator}</th>
+                {headerOptions.map((item, index) => {
                   return (
-                    <th key={`1-${index}-${value}`} className="p-3">
-                      {showLabel(item)}
+                    <th key={`1-${index}-${data}`} className="p-3">
+                      <input
+                        className="bg-transparent text-center w-[80px]"
+                        value={hederValue[item]}
+                        name={item}
+                        onChange={onChange}
+                      />
                     </th>
                   );
                 })}
               </tr>
             </thead>
             <tbody>
-              {options.map((bodyItem, index) => {
+              {bodyOptions.map((bodyItem, index) => {
                 return (
-                  <tr key={`2-${index}-${value}`}>
-                    <th className="px-3">{showLabel(bodyItem)}</th>
-                    {options.map((headerItem, titleIndex) => {
-                      return <td key={`3-${headerItem}-${titleIndex}-${value}`}>{showResult(bodyItem, headerItem, value)}</td>;
+                  <tr key={`2-${index}-${data}`}>
+                    <th className="px-3">
+                      <input
+                        className="bg-transparent text-center w-[80px]"
+                        value={bodyValue[bodyItem]}
+                        name={bodyItem}
+                        onChange={onChange}
+                      />
+                    </th>
+                    {headerOptions.map((headerItem, titleIndex) => {
+                      return (
+                        <td key={`3-${headerItem}-${titleIndex}-${data}`}>
+                          {showResult(data.body[bodyItem], data.header[headerItem], data.operator)}
+                        </td>
+                      );
                     })}
                   </tr>
                 );
